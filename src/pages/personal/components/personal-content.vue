f0b39647862026ba24d9daf9900fd80a3d3b8d0a
miffy5821 <chenfei5821@gmail.com>
2020-07-10 21:56:26 +0800

    个人中心

--- src/pages/personal/components/personal-content.vue
+++ src/pages/personal/components/personal-content.vue
@@ -1,60 +1,118 @@
 <template>
   <div class="personal">
-   <div class="personalBox">
-     <div class="left">
-       <div class="leftLogo"></div>
-       <div class="personal-list"></div>
-       <div class="app-code">
-         <img class="code" src="">
-       </div>
-     </div>
-     <div class="center">
-       <div class="tg">
-         <div class="tg-icon"><img class="iconTg" src="" alt=""></div>
-         <p class="tgContent">尊敬的澳门银河贵宾，由于线上充值会员多通道拥挤，充值成功率降低，建议贵宾使用公司线下转卡方式充值，
-           笔笔入款1.5%,感谢您对我司一直以来的支持，祝您游戏愉快~</p>
-       </div>
-       <div class="ym">
-         <h1 class="ymContent">请牢记我们官方唯一的域名导航 www.888y.com</h1>
-       </div>
-       <div class="switch">
-         <button class="switchButton">切换至手动转账</button>
-       </div>
-       <div class="asset">
-        <div class="assetName">我的资产</div>
-       </div>
-       <div class="asset-case">
-         <div class="asset-left">
-           <h1>0.00</h1>
-           <div>
-             <button>提款</button>
-             <button>存款</button>
-           </div>
-           <div ></div>
-         </div>
-         <div class="asset-right">
-           <div>
-             <button>刷新</button>
-           </div>
-           <div>
-             <span>中心钱包</span>
-             <span>￥0.00</span>
-             <span>一键回收</span>
-           </div>
-           <div>
-             <span>积分总额</span>
-             <span>0</span>
-           </div>
-         </div>
-       </div>
-       <div class="assetDistribute">
-         <div class="DistributeName">财产分布</div>
-       </div>
-       <div class="Distribute">
-         <div class="Distribute-ctn"></div>
-       </div>
-     </div>
-   </div>
+    <div class="personalBox">
+      <div class="left">
+        <div class="leftLogo">
+          <img class="personal-logo" src="/static/personal/logo.png"/>
+          <h3 class="user">ceto5821,欢迎您!</h3>
+          <div class="status">
+            <p>认证状态：<span class="statusBox"><img class="status-icon" src="/static/personal/phone.png" alt=""></span>
+              <span class="statusBox"><img class="status-icon" src="/static/personal/phone.png" alt=""></span></p>
+          </div>
+        </div>
+        <div class="personal-list">
+          <div class="personal-list">
+            <ul class="nav">
+              <li  class="nav-color">
+                <router-link to="/personal-content">
+                  <img class="nav-icon " src="/static/personal/phone.png"/> <span class="active">额度转换</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/deposit">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>存款专区</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/withdrawal">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>取款专区</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/capital">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>资金记录</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/bet">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>投注记录</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/agency">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>高级代理人</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/personal-data">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>个人资料</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/information">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>信息公告</span>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/change-password">
+                  <img class="nav-icon" src="/static/personal/phone.png"/> <span>修改密码</span>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+          </div>
+        <div class="app-code">
+          <img class="code" src="/static/personal/app-code.png">
+          <p>扫码下载APP</p>
+        </div>
+      </div>
+      <div class="center">
+        <div class="tg">
+          <div class="tg-icon"><img class="iconTg" src="/static/personal/phone.png" alt=""></div>
+          <p class="tgContent">尊敬的澳门银河贵宾，由于线上充值会员多通道拥挤，充值成功率降低，建议贵宾使用公司线下转卡方式充值，
+            笔笔入款1.5%,感谢您对我司一直以来的支持，祝您游戏愉快~</p>
+        </div>
+        <div class="ym">
+          <p class="ymContent">请牢记我们官方唯一的域名导航 www.888y.com</p>
+        </div>
+        <div class="switch">
+          <button class="switchButton">切换至手动转账</button>
+        </div>
+        <div class="asset">
+          <div class="assetName">我的资产</div>
+        </div>
+        <div class="asset-case">
+          <div class="asset-left">
+            <div class="tex1">0.00</div>
+            <div class="asset-btn">
+              <button class="asset-btn1">提款</button>
+              <button class="asset-btn1 btnColor">存款</button>
+            </div>
+            <div></div>
+          </div>
+          <div class="asset-right">
+            <div class="asset-">
+              <button class="asset-btn2">刷新</button>
+            </div>
+            <div class="asset-wallet">
+              <span class="wallet">中心钱包</span>
+              <span class="sum">￥0.00</span>
+              <span class="recycle">一键回收</span>
+            </div>
+            <div class="asset-wallet">
+              <span class="integral">积分总额</span>
+              <span class="total">0</span>
+            </div>
+          </div>
+        </div>
+        <div class="assetDistribute">
+          <div class="assetName">财产分布</div>
+        </div>
+        <div class="Distribute">
+          <div class="Distribute-ctn"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -62,51 +120,378 @@
     name: 'personalContent'
   }
 </script>
-<style >
-  .personal{
+<style>
+  .personal {
     width: 100%;
     height: 1500px;
     display: flex;
-    background-color: #eaeaea;
+    cursor:pointer;
+    background-color: #f7f6f4;
   }
-  .personalBox{
+
+  .personalBox {
     width: 1200px;
     height: 1150px;
     margin-top: 20px;
     margin-right: auto;
     margin-left: auto;
     display: flex;
-    border: 1px solid black;
+    border: 1px solid #eaeaea;
   }
-.left{
-  width: 196px;
-  height: 100%;
-  border: 1px solid black;
-}
-.leftLogo{
-  width: 196px;
-  height: 160px;
-  border-bottom: 1px solid black ;
-  margin: 20px 0;
-}
-.personal-list{
-  width: 196px;
-  height: 427px;
-  padding: 0 0 20px;
-  border-bottom: 1px solid black ;
-}
-.app-code{
-  width: 196px;
-  height: 234px;
-  padding: 20px 0;
-}
-.code{
-  width: 180px;
-  height: 180px;
-}
-  .center{
+
+  .left {
+    width: 196px;
+    height: 100%;
+    background: white;
+    border: 1px solid #eaeaea;
+  }
+
+  .leftLogo {
+    width: 196px;
+    height: 180px;
+    border-bottom: 1px solid #eaeaea;
+    margin: 20px 0;
+  }
+
+  .personal-logo {
+    width: 80px;
+    height: 80px;
+    border: 4px solid #eaeaea;
+    border-radius: 50%;
+  }
+
+  .user {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    font-size: 16px;
+    margin-top: 8px;
+    font-weight: 700;
+  }
+
+  .status {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    margin-top: 8px;
+  }
+
+  .status p {
+    width: auto;
+    height: 25px;
+    color: black;
+    line-height: 25px;
+    font-size: 13px;
+  }
+
+  .statusBox {
+    width: 18px;
+    height: 18px;
+    padding: 5px;
+    margin-left: 8px;
+    background-color: #eaeaea;
+    border-radius: 50px;
+  }
+
+  .status-icon {
+    width: 18px;
+    height: 18px;
+  }
+
+  .personal-list {
+    width: 196px;
+    height: 427px;
+    cursor:pointer;
+    border-bottom: 1px solid #eaeaea;
+  }
+
+  .nav {
+    width: 196px;
+    height: 427px;
+    margin-top: -20px;
+  }
+
+  .nav li {
+    height: 45px;
+    width: 196px;
+    font-size: 14px;
+    line-height: 45px;
+  }
+  a{
+    list-style: none;
+    text-decoration: none;
+    color:  gray;
+  }
+  .nav-icon {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+
+  .nav-color {
+
+    transition: all .3s;
+    background-color: #f2f2f2;
+  }
+  .active{
+    color: #c8a675;
+  }
+  /*.text{*/
+  /*margin-left: 10px;*/
+  /*font-size:14px;*/
+  /*height: 45px;*/
+  /*line-height: 45px;*/
+  /*width: auto;*/
+  /*}*/
+  .app-code {
+    width: 196px;
+    height: 234px;
+    padding: 20px 0;
+  }
+
+  .code {
+    width: 180px;
+    height: 180px;
+  }
+
+  .app-code p {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    font-size: 14px;
+    color: black;
+    text-align: center;
+  }
+
+  .center {
     width: 1000px;
     height: 1000px;
-    border: 1px solid black;
+    background: white;
+    border: 1px solid #eaeaea;
+  }
+
+  .tg {
+    width: 960px;
+    height: 25px;
+    display: flex;
+    justify-content: center;
+    background: #eaeaea;
+    padding: 20px 20px;
+  }
+
+  .tg-icon {
+    width: 30px;
+    height: 30px;
+    background: #c8a675;
+    border-radius: 50%;
+  }
+
+  .iconTg {
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    padding-top: 5px;
+  }
+
+  .tgContent {
+    width: 900px;
+    height: 25px;
+    line-height: 25px;
+    font-size: 14px;
+    margin-left: 20px;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .ym {
+    width: 978px;
+    height: 38px;
+    margin: 10px;
+  }
+
+  .ymContent {
+    width: 978px;
+    height: 38px;
+    text-align: left;
+    line-height: 38px;
+    font-size: 36px;
+    color: red;
+
+  }
+
+  .switch {
+    width: 998px;
+    height: 35px;
+  }
+
+  .switchButton {
+    width: 160px;
+    height: 30px;
+    color: goldenrod;
+    border: 1px solid goldenrod;
+    border-radius: 5px;
+    text-align: center;
+    margin-left: 750px;
+    background: white;
+    font-size: 14px;
+  }
+
+  .asset {
+    width: 998px;
+    height: 50px;
+  }
+
+  .assetName {
+    height: 40px;
+    background: linear-gradient(90deg, #f2f2f2, #fff);
+    line-height: 40px;
+    padding: 0 30px;
+    width: 200px;
+    text-align: left;
+    padding-left: -30px;
+    font-size: 15px;
+    font-weight: 700;
+    color: #666;
+  }
+
+  .asset-case {
+    width: 998px;
+    height: 160px;
+    padding: 10px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .asset-left {
+    width: 400px;
+    height: 160px;
+    padding: 20px 30px;
+    border-right: 1px solid #eaeaea;
+  }
+
+  .tex1 {
+    width: 380px;
+    height: 80px;
+    font-size: 50px;
+    font-weight: 700;
+    padding-left: 10px;
+    text-align: left;
+    color: black;
+  }
+
+  .asset-btn {
+    width: 399px;
+    height: 40px;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .asset-btn1 {
+    width: 130px;
+    height: 40px;
+    margin: 0 10px;
+    color: #c8a675;
+    padding: 0 15px;
+    text-align: center;
+    line-height: 40px;
+    background: white;
+    font-size: 14px;
+    border: 1px solid #c8a675;
+    border-radius: 5px;
+  }
+
+  .btnColor {
+    background: #c8a675;
+    color: white;
+  }
+
+  .asset-right {
+    width: 500px;
+    height: 160px;
+  }
+
+  .asset- {
+    width: 500px;
+    height: 50px;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .asset-btn2 {
+    width: 60px;
+    height: 35px;
+    font-size: 14px;
+    background: #c8a675;
+    border: none;
+    border-radius: 5px;
+    margin-right: 20px;
+  }
+
+  .asset-wallet {
+    width: 238px;
+    height: 50px;
+    line-height: 50px;
+    font-size: 14px;
+    display: flex;
+    margin-left: 30px;
+    justify-content: flex-start;
+
+  }
+
+  .wallet {
+    width: 56px;
+    height: 19px;
+    margin-right: 20px;
+    font-size: 14px;
+  }
+
+  .sum {
+    width: 42px;
+    font-size: 14px;
+    height: 19px;
+    margin-right: 20px;
+  }
+
+  .recycle {
+    width: 80px;
+    height: 28px;
+    line-height: 28px;
+    border: 1px solid #c8a675;
+    border-radius: 5px;
+    font-size: 13px;
+    margin-top: 10px;
+    color: #c8a675;
+  }
+
+  .integral {
+    width: 56px;
+    height: 19px;
+    margin-right: 20px;
+    font-size: 14px;
+  }
+
+  .total {
+    width: 10px;
+    height: 19px;
+    font-size: 14px;
+  }
+
+  .assetDistribute {
+    width: 998px;
+    height: 300px;
+    border: 1px dashed silver;
+    margin-top: 60px;
+    padding-top: 30px;
+  }
+
+  .Distribute {
+
+  }
+
+  .Distribute-ctn {
+
   }
 </style>
---
+++ src/pages/personal/components/withdrawal.vue
@@ -0,0 +1,261 @@
+<template>
+  <div class="personal">
+    <div class="personalBox">
+      <div class="left">
+        <div class="leftLogo">
+          <img class="personal-logo" src="/static/personal/logo.png"/>
+          <h3 class="user">ceto5821,欢迎您!</h3>
+          <div class="status">
+            <p>认证状态：<span class="statusBox"><img class="status-icon" src="/static/personal/phone.png" alt=""></span>
+              <span class="statusBox"><img class="status-icon" src="/static/personal/phone.png" alt=""></span></p>
+          </div>
+        </div>
+        <div class="personal-list">
+          <ul class="nav">
+            <li>
+              <router-link to="/personal-content">
+                <img class="nav-icon " src="/static/personal/phone.png"/> <span>额度转换</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/deposit">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>存款专区</span>
+              </router-link>
+            </li>
+            <li  class="nav-color">
+              <router-link to="/withdrawal">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span  class="active">取款专区</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/capital">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>资金记录</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/bet">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>投注记录</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/agency">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>高级代理人</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/personal-data">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>个人资料</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/information">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>信息公告</span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/change-password">
+                <img class="nav-icon" src="/static/personal/phone.png"/> <span>修改密码</span>
+              </router-link>
+            </li>
+          </ul>
+
+        </div>
+
+        <div class="app-code">
+          <img class="code" src="/static/personal/app-code.png">
+          <p>扫码下载APP</p>
+        </div>
+      </div>
+      <div class="center">
+        <div class="tg">
+          <div class="tg-icon"><img class="iconTg" src="/static/personal/phone.png" alt=""></div>
+          <p class="tgContent">尊敬的澳门银河贵宾，由于线上充值会员多通道拥挤，充值成功率降低，建议贵宾使用公司线下转卡方式充值，
+            笔笔入款1.5%,感谢您对我司一直以来的支持，祝您游戏愉快~</p>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'withdrawal'
+  }
+</script>
+<style>
+  .personal {
+    width: 100%;
+    height: 1500px;
+    display: flex;
+    cursor:pointer;
+    background-color: #f7f6f4;
+  }
+
+  .personalBox {
+    width: 1200px;
+    height: 1150px;
+    margin-top: 20px;
+    margin-right: auto;
+    margin-left: auto;
+    display: flex;
+    border: 1px solid #eaeaea;
+  }
+
+  .left {
+    width: 196px;
+    height: 100%;
+    background: white;
+    border: 1px solid #eaeaea;
+  }
+
+  .leftLogo {
+    width: 196px;
+    height: 180px;
+    border-bottom: 1px solid #eaeaea;
+    margin: 20px 0;
+  }
+
+  .personal-logo {
+    width: 80px;
+    height: 80px;
+    border: 4px solid #eaeaea;
+    border-radius: 50%;
+  }
+
+  .user {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    text-align: center;
+    font-size: 16px;
+    margin-top: 8px;
+    font-weight: 700;
+  }
+
+  .status {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    margin-top: 8px;
+  }
+
+  .status p {
+    width: auto;
+    height: 25px;
+    color: black;
+    line-height: 25px;
+    font-size: 13px;
+  }
+
+  .statusBox {
+    width: 18px;
+    height: 18px;
+    padding: 5px;
+    margin-left: 8px;
+    background-color: #eaeaea;
+    border-radius: 50px;
+  }
+
+  .status-icon {
+    width: 18px;
+    height: 18px;
+  }
+
+  .personal-list {
+    width: 196px;
+    height: 427px;
+    cursor:pointer;
+    border-bottom: 1px solid #eaeaea;
+  }
+
+  .nav {
+    width: 196px;
+    height: 427px;
+    margin-top: -20px;
+  }
+
+  .nav li {
+    height: 45px;
+    width: 196px;
+    font-size: 14px;
+    line-height: 45px;
+  }
+
+  .nav-icon {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+
+  .nav-color {
+
+    transition: all .3s;
+    background-color: #f2f2f2;
+  }
+  .active{
+    color: #c8a675;
+  }
+
+  .app-code {
+    width: 196px;
+    height: 234px;
+    padding: 20px 0;
+  }
+
+  .code {
+    width: 180px;
+    height: 180px;
+  }
+
+  .app-code p {
+    width: 196px;
+    height: 25px;
+    line-height: 25px;
+    font-size: 14px;
+    color: black;
+    text-align: center;
+  }
+
+  .center {
+    width: 1000px;
+    height: 1000px;
+    background: white;
+    border: 1px solid #eaeaea;
+  }
+
+  .tg {
+    width: 960px;
+    height: 25px;
+    display: flex;
+    justify-content: center;
+    background: #eaeaea;
+    padding: 20px 20px;
+  }
+
+  .tg-icon {
+    width: 30px;
+    height: 30px;
+    background: #c8a675;
+    border-radius: 50%;
+  }
+
+  .iconTg {
+    width: 20px;
+    height: 20px;
+    text-align: center;
+    padding-top: 5px;
+  }
+
+  .tgContent {
+    width: 900px;
+    height: 25px;
+    line-height: 25px;
+    font-size: 14px;
+    margin-left: 20px;
+    text-align: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+</style>
